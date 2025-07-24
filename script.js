6b53c55eb2574f3774965fab2537b3cf5003dd39
RahulA075 <rahularu2005@gmail.com>
2025-07-24 12:16:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
     document.getElementById('form-status').textContent = "Thank you! Your message has been sent.";
     this.reset();
 });
+
