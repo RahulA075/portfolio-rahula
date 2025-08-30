c257bca70837bd9e24e8b154f2b029b994aa71e8
RahulA075 <rahularu2005@gmail.com>
2025-08-30 13:01:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Simple contact form feedback (for demo)
 document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
     document.getElementById('form-status').textContent = "Thank you! Your message has been sent.";
     this.reset();
 });
+
